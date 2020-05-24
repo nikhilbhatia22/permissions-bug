2d4adff84d81a640274a848c4696bbb8bafa330c
Nikhil Bhatia <nikhilbhatia002@gmail.com>
2020-05-25 01:48:46 +0530

    Added bug route

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('bug', 'HomeController@bug')->name('bug')->middleware('auth');
+
