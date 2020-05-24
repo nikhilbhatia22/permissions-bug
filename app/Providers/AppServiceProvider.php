a546242d21c1dacb126f78d4e58b279a3bbf7ad2
Nikhil Bhatia <nikhilbhatia002@gmail.com>
2020-05-25 01:44:19 +0530

    Support for older MYSQL

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        \Schema::defaultStringLength(191);   //For lower mysql versions
     }
 }
